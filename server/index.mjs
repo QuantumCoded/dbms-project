f1058bac37087654b97352f3ca36334a5e59b1a2
QuantumCoded <QuantumCoded@users.noreply.github.com>
2023-11-22 12:30:26 -0600

    basic musicbrainz search support

--- server/index.mjs
+++ server/index.mjs
@@ -1,9 +1,16 @@
 import express from "express";
+import {MusicBrainzApi} from "musicbrainz-api";
 import path from "path";
-import { fileURLToPath } from 'url';
+import {fileURLToPath} from 'url';
 
 const app = express();
 const port = 3000;
+
+const mb_api = new MusicBrainzApi({
+  appName: "dbms-project",
+  appVersion: "1.0.0",
+  appContactInfo: "null@void.com",
+});
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -13,6 +20,23 @@ const root_dir = path.join(__dirname, "../client");
 app.get("/", (_req, res) => {
   res.sendFile(path.join(root_dir, "index.html"));
 });
+
+app.get("/search", (req, res) => {
+  let query = req.query.q;
+
+  console.log("querying musicbrainz:", JSON.stringify(query));
+
+  mb_api.search('release', {query}).then(
+    search_result => {
+      res.header("Content-Type", "text/plain").send(JSON.stringify(search_result, null, 2));
+    },
+
+    err => {
+      res.header("Content-Type", "text/plain").send("something went wrong " + JSON.stringify(err, null, 2));
+
+    }
+  );
+})
 
 app.get("/main.js", (_req, res) => {
   res.sendFile(path.join(root_dir, "main.js"));
